dd79f648dddeb2b80f0f034803d7b4af6c4b8b11
Samuel Gomes <samuel.gomes@onerf.com.br>
2023-03-03 16:51:59 -0300

    test c++ openwrt

---
+++ gateway/reading_routes.cpp
@@ -0,0 +1,14 @@
+/**
+
+https://forum.openwrt.org/t/solved-how-to-cross-compile-c-program-for-openwrt/66343/2
+
+*/
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+  cout << "Hello World! ";
+  cout << "I'm a C++ program";
+  return 0;
+}
