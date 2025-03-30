00d5fa067eb1f33be9348dee79d8aabd03edd523
Samuel Gomes <samuel.huarachi@gmail.com>
2025-03-29 23:56:03 -0300

    242 lesson

--- udemy_course_basics_cpp/20_the_standard_template_library/242_creating_a_generic_array_template_class/main.cpp
+++ udemy_course_basics_cpp/20_the_standard_template_library/242_creating_a_generic_array_template_class/main.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 #include <string>
 
-class Array {
+template <int N> class Array {
     int size{N};
     int values[N];
 
-    friend std::ostream &operator<<(std::ostream &os, const Array &arr) {
+    friend std::ostream &operator<<(std::ostream &os, const Array<N> &arr) {
         os << "[";
         for (const auto &val : arr.values)
             os << val << " ";
@@ -14,4 +14,21 @@ class Array {
 
         return os;
     }
-}
+
+  public:
+    Array() = default;
+    Array(int init_val) {
+        for (auto &item : values)
+            item = init_val;
+    }
+
+    void fill(int val) {
+        for (auto &item : values)
+            item = val;
+    }
+
+    int get_site() const { return size; }
+
+    // overlaoded subuscript operator for easy use
+    int &operator[](int index) { return value[index]; }
+};
