74ecda977a662405f05172e1cd1434b8bc9e94cc
Samuel Gomes <samuel.huarachi@gmail.com>
2025-03-30 00:02:09 -0300

    still lesson 242, about generic in classes

--- udemy_course_basics_cpp/20_the_standard_template_library/242_creating_a_generic_array_template_class/main.cpp
+++ udemy_course_basics_cpp/20_the_standard_template_library/242_creating_a_generic_array_template_class/main.cpp
@@ -27,8 +27,32 @@ template <int N> class Array {
             item = val;
     }
 
-    int get_site() const { return size; }
+    int get_size() const { return size; }
 
     // overlaoded subuscript operator for easy use
     int &operator[](int index) { return value[index]; }
 };
+
+int main() {
+
+    Array<5> nums;
+    std::cout << "The size of nums is:" << num.get_size() << std::endl;
+    std::cout << nums << std::endl;
+
+    nums.fill(0);
+    std::cout << "The size of nums is: " << num.get_size() << std::endl;
+    std::cout << nums << std::endl;
+
+    nums.fill(10);
+    std::cout << nums << std::endl;
+
+    nums[0] = 1000;
+    nums[3] = 2000;
+    std::cout << nums << std::endl;
+
+    Array<100> nums2{1};
+    std::cout << "The size of nums2 is: " << nums2.get_size() << std::endl;
+    std::cout << nums2 << std::endl;
+
+    return 0;
+}
