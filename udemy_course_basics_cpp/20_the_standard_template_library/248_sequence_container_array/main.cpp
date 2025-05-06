dd7399dd3080594d7407da1ae918c0949b195623
Samuel Gomes <samuel.huarachi@gmail.com>
2025-05-05 23:47:59 -0300

    in progress - 248 - sequence container array

--- udemy_course_basics_cpp/20_the_standard_template_library/248_sequence_container_array/main.cpp
+++ udemy_course_basics_cpp/20_the_standard_template_library/248_sequence_container_array/main.cpp
@@ -34,6 +34,58 @@ void test1() {
     std::cout << "Back of arr2: " << arr2.back() << std::endl;   // 50
 }
 
+void test2() {
+    std::cout << "\nTest2 =====================" << std::endl;
+    std::array<int, 5> arr1{1, 2, 3, 4, 5};
+    std::array<int, 5> arr2{10, 20, 30, 40, 50};
+
+    display(arr1);
+    display(arr2);
+
+    arr1.fill(0);
+
+    display(arr1);
+    display(arr2);
+
+    arr1.swap(arr2);
+
+    display(arr1);
+    display(arr2);
+}
+
+void test3() {
+    std::cout << "\nTest3 ==========================" << std::endl;
+    std::array<int, 5> arr1{1, 2, 3, 4, 5};
+
+    int *ptr = arr1.data();
+    std::cout << ptr << std::endl;
+    *ptr = 10000;
+
+    display(arr1);
+}
+
+void test4() {
+    std::cout << "\nTest4 =================" << std::endl;
+
+    std::array<int, 5> arr1{2, 1, 4, 5, 3};
+    display(arr1);
+
+    std::sort(arr1.begin(), arr1.end());
+    display(arr1);
+}
+
+void test5() {
+    std::cout << "\nTest5 ==================" << std::endl;
+
+    std::array<int, 5> arr1{2, 1, 4, 5, 3};
+
+    std::array<int, 5>::iterator min_num =
+        std::min_element(arr1.begin(), arr1.end());
+    auto max_num = std::max_element(arr1.begin(), arr1.end());
+
+    std::cout << "min: " << *min_num << " , max: " << *max_num << std::endl;
+}
+
 int main() {
 
     test1();
