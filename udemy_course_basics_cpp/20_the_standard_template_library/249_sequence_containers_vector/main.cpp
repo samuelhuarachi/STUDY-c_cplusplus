6329d651362e480f2228185de7b0e37409d9b9a9
Samuel Gomes <samuel.huarachi@gmail.com>
2025-06-24 23:28:14 -0300

    in progress - Sequence container vector

--- udemy_course_basics_cpp/20_the_standard_template_library/249_sequence_containers_vector/main.cpp
+++ udemy_course_basics_cpp/20_the_standard_template_library/249_sequence_containers_vector/main.cpp
@@ -27,3 +27,82 @@ void display2(const std::vector<int> &vec) {
     std::for_each(vec.begin(), vec.end(), [](int x) { std::cout << x << " "; });
     std::cout << "]" << std::endl;
 }
+
+template <typename T> void display(const std::vector<T> &vec) {
+    std::cout << "[";
+    for (const auto &elem : vec)
+        std::cout << elem << " ";
+    std::cout << "]" << std::endl;
+}
+
+void test1() {
+    std::cout << "\nTest1 ==============" << std::endl;
+
+    std::vector<int> vec{1, 2, 3, 4, 5};
+    display(vec);
+
+    vec = {2, 4, 5, 6};
+    display2(vec);
+
+    std::vector<int> vec1(10, 100);
+    display(vec1);
+}
+
+void test2() {
+    std::cout << "\nTest2 ==================" << std::endl;
+
+    std::vector<int> vec{1, 2, 3, 4, 5};
+    display(vec);
+    std::cout << "\nvec size: " << vec.size() << std::endl;
+    std::cout << "vec max size: " << vec.max_size() << std::endl;
+    std::cout << "vec capacity: " << vec.capacity() << std::endl;
+
+    vec.push_back(6);
+    display(vec);
+    std::cout << "\nvec size: " << vec.size() << std::endl;
+    std::cout << "vec max size: " << vec.max_size() << std::endl;
+    std::cout << "vec capacity: " << vec.capacity() << std::endl;
+
+    vec.shrink_to_fit();
+    display(vec);
+    std::cout << "\nvec size: " << vec.size() << std::endl;
+    std::cout << "vec max size: " << vec.max_size() << std::endl;
+    std::cout << "vec capacity: " << vec.capacity() << std::endl;
+
+    vec.reserve(100);
+    display(vec);
+    std::cout << "\nvec size: " << vec.size() << std::endl;
+    std::cout << "vec max size: " << vec.max_size() << std::endl;
+    std::cout << "vec capacity: " << vec.capacity() << std::endl;
+}
+
+void test3() {
+    std::cout << "\nTest3 =====================" << std::endl;
+    std::vector<int> vec{1, 2, 3, 4, 5};
+    display(vec);
+
+    vec[0] = 100;
+    vec.at(1) = 200;
+
+    display(vec);
+}
+
+void test4() {
+    std::cout << "Test4 =====================" << std::endl;
+    std::vector<Person> stooges;
+
+    Person p1{"Larry", 18};
+    display(stooges);
+
+    stooges.push_back(p1);
+    display(stooges);
+
+    stooges.push_back(Person{"Moe", 25});
+    display(stooges);
+
+    stooges.emplace_back("Curly", 30); // Use emplace_back!!!!
+
+    display(stooges);
+}
+
+void test5() {}
